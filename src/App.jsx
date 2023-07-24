f134cbcd891c4c3c012822c9c80874cecdc754ed
UmidIsanov <isanovumid8@gmail.com>
2023-07-25 01:39:48 +0500

    adding new components to view listOfMeals

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Header from "./components/Layout/Header";
+import MealsList from "./components/Meals/MealsList";
 function App() {
   return (
    <React.Fragment>
     <Header/>
+    <main>
+      <MealsList/>
+    </main>
    </React.Fragment>
   );
 }
---
+++ src/components/Meals/MealsList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Meals from './Meals'
+import PromoText from './PromoText'
+
+const MealsList = () => {
+  return (
+    <div><Meals/>
+    <PromoText/>
+    </div>
+  )
+}
+
+export default MealsList
