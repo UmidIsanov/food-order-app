c4e6222be4bcb1e7b46c602689a3044e637c0df2
UmidIsanov <isanovumid8@gmail.com>
2023-07-25 23:06:04 +0500

    addeed Modal component , bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "./components/Layout/Header";
 import MealsList from "./components/Meals/MealsList";
+import Cart from "./components/Cart/Cart";
 function App() {
   return (
    <React.Fragment>
     <Header/>
+    <Cart/>
+
     <main>
       <MealsList/>
     </main>
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,23 +1,27 @@
-import styles from './Cart.module.css'
+import Modal from '../UI/Modal'; // Make sure the correct file path is used
+import styles from './Cart.module.css';
 
 const Cart = (props) => {
   
-  const cartItems = <ul className={styles['cart-items']}> 
-    {[{id: 'm1', name: 'sushi', amount: 2, price: 10.99} ].map(item => <li>{item.name}</li> )}</ul>
-    return (
-    <div>
-        {cartItems}
-        <div className={styles.total}>
-            <span>Итого</span>
-            <span>49.99</span>
-        </div>
-        <div className={styles.actions}> 
+  const cartItems = (
+    <ul className={styles['cart-items']}>
+      {[{id: 'm1', name: 'sushi', amount: 2, price: 10.99}].map(item => <li key={item.id}>{item.name}</li>)}
+    </ul>
+  );
+
+  return (
+    <Modal>
+      {cartItems}
+      <div className={styles.total}>
+        <span>Итого</span>
+        <span>49.99</span>
+      </div>
+      <div className={styles.actions}> 
         <button className={styles['button--alt']}>Закрыть</button>
         <button className={styles['button--button']}>Заказать</button>
-
-        </div>
-    </div>
-  )
+      </div>
+    </Modal>
+  );
 }
 
-export default Cart
+export default Cart;
